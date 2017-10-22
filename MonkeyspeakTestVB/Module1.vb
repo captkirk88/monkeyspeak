df2f4c71f04e281780d0e570cdf1e6139a3577ea
Kirk MacMillan <kirkmacmillan88@gmail.com>
2017-10-22 02:00:31 -0600

    ABCDEFGHIJKLMNOPQRSTUVWXYZ

--- MonkeyspeakTestVB/Module1.vb
+++ MonkeyspeakTestVB/Module1.vb
@@ -23,61 +23,19 @@ Public Module Module1
     End Function
 
     Sub Main()
-        Dim testScript As String = <a>
-(0:0) when the script starts,
-	(5:100) set variable %file to {test.txt}.
-
-(0:0) when the script starts,
-	(1:200) and the file {%file} exist,
-		(5:202) delete file {%file}.
-		(5:203) create file {%file}.
-
-(0:0) when the script starts,
-	(1:200) and the file {%file} exists,
-	(1:203) and the file {%file} can be written to,
-		(5:200) append {Hello World from Monkeyspeak!} to file {%file}.
-        (5:200) append {End of file... .. .. .} to file {%file}.
-
-(0:0) when the script starts,
-	(5:150) take variable %test and add 2 to it.
-	(5:102) print {%test} to the console.
-    (5:100) set variable %test to {test}.
-    (5:102) print {%test} to the console.
-
-(0:1000) test cause with reflection,
-	(5:1001) test print {Hello World with Reflection!!}.
-</a>.Value
-
         Dim engine = New Monkeyspeak.MonkeyspeakEngine()
-        Console.WriteLine("Running " & vbCrLf & testScript)
+        Console.WriteLine("Running")
 
         Dim start = Stopwatch.StartNew()
-        Dim page As Monkeyspeak.Page = engine.LoadFromString(testScript)
+        Dim page As Monkeyspeak.Page = engine.LoadFromFile("testBIG.ms")
 
         page.LoadAllLibraries()
 
         page.Execute()
-        page.Execute(1000)
+        start.Stop()
         Console.WriteLine("Done! Executed in " & start.Elapsed.ToString())
         Console.WriteLine("Press any key to continue...")
         Console.ReadKey()
     End Sub
 
 End Module
-
-Class TestLibrary
-
-    <Monkeyspeak.TriggerHandler(Monkeyspeak.TriggerCategory.Cause, 1000, "(0:1000) test cause with reflection,")>
-    Shared Function TestVBReflectionTriggerHandler(reader As Monkeyspeak.TriggerReader) As Boolean 'IMPORTANT label return with As Boolean
-        Return True
-    End Function
-
-    <Monkeyspeak.TriggerHandler(Monkeyspeak.TriggerCategory.Effect, 1001, "(5:1001) test print {...}.")>
-    Shared Function TestVBReflectionTriggerHandler2(reader As Monkeyspeak.TriggerReader) As Boolean 'IMPORTANT label return with As Boolean
-        If reader.PeekString Then
-            Console.WriteLine(reader.ReadString())
-        End If
-        Return True
-    End Function
-
-End Class
